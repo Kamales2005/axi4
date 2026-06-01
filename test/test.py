8dda20d7ec93f7b0cc0d4c561326e7fd59186236
Kamales2005 <kamales5468@gmail.com>
2026-06-01 22:58:52 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -12,22 +12,27 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
+    # Reset
     dut.ena.value = 1
     dut.rst_n.value = 0
     dut.ui_in.value = 0
+    dut.uio_in.value = 0
 
     await ClockCycles(dut.clk, 5)
 
     dut.rst_n.value = 1
 
-   dut.ui_in.value = 0x55
-await ClockCycles(dut.clk, 2)
-assert int(dut.uo_out.value) == 0x55
+    # Test 1
+    dut.ui_in.value = 0x55
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.uo_out.value) == 0x55
 
+    # Test 2
     dut.ui_in.value = 0xAA
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 0xAA
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.uo_out.value) == 0xAA
 
+    # Test 3
     dut.ui_in.value = 0xF0
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 0xF0
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.uo_out.value) == 0xF0
